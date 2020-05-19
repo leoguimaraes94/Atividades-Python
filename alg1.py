eaa228488a40cd8b59e266cfc3ed78db22eba5b5
Léo Mendes <54195469+leoguimaraes94@users.noreply.github.com>
2020-05-19 20:21:15 -0300

    Add files via upload

---
+++ alg1.py
@@ -0,0 +1,22 @@
+#Disciplina :  [Lógica de Programação e Estruturas de Dados] 
+#Leonardo de Oliveira Mendes
+'''Faça um algoritmo que receba dois números e ao final mostre a soma,
+   subtração, multiplicação e a divisão dos números lidos.'''
+
+
+print("digite um número: ")
+a = float(input())
+print("digite um segundo número: ")
+b = float(input())
+soma = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print(f"a soma dos números digitados é: {soma}")
+print(F"a subtração dos números digitados é: {sub}")
+print(f"a multiplicação dos números digitados é: {mul}")
+print(f"a divisão dos números digitados é: {div}")
+
+
+
+
