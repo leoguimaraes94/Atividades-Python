b03eecbadba2be69d50b5f488db45da7dc9b401e
Léo Mendes <54195469+leoguimaraes94@users.noreply.github.com>
2020-06-30 23:39:07 -0300

    Add files via upload

---
+++ 1-exercício.py
@@ -0,0 +1,49 @@
+#Variáveis
+
+N1=int
+N2=int
+
+#Início
+
+print ("Olá, seja bem-vindo! Vamos práticar as quatro operações matemáticas!")
+print ("")
+
+#Soma
+print ("Abaixo digite o primeiro número e o segundo para fazermos a soma.")
+print ("")
+N1=int(input("Digite o primeiro número: "))
+print("")
+N2=int(input("Digite o segundo número: "))
+print("")
+print ("O resultado é: ",N1+N2)
+print("")
+
+#Subtração
+print ("Abaixo digite o primeiro número e o segundo para fazermos a subtração.")
+print ("")
+N1=int(input("Digite o primeiro número: "))
+print("")
+N2=int(input("Digite o segundo número: "))
+print("")
+print ("O resultado é: ",N1-N2)
+print("")
+
+#Divisão
+print ("Abaixo digite o primeiro número e o segundo para fazermos a divisão.")
+print("")
+N1=int(input("Digite o primeiro número: "))
+print("")
+N2=int(input("Digite o segundo número: "))
+print("")
+print ("O resultado é: ", N1/N2)
+print("")
+
+#Multiplicação
+print ("Abaixo digite o primeiro número e o segundo para fazermos a multiplicação:")
+print("")
+N1=int(input("Digite o primeiro número: "))
+print("")
+N2=int(input("Digite o segundo número: "))
+print("")
+print("O resultado é: ", N1*N2)
+print("")
